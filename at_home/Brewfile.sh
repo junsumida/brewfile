ad8ad01222f1d559353c965c914b92c7354f7021
Jun Sumida <sekitoh.rh@gmail.com>
2014-10-31 19:14:42 +0900

    renew Brewfile as Brewfile.sh

---
+++ at_home/Brewfile.sh
@@ -0,0 +1,27 @@
+brew update
+brew upgrade
+brew tap phinze/homebrew-cask
+brew install brew-cask
+
+brew install redis
+brew install mysql --cc=llvm-gcc
+brew install tree
+brew install tig
+brew install zsh
+brew install hub
+brew install the_silver_searcher
+brew install mysql
+brew install memcached
+brew install colordiff
+
+brew # Ruby
+brew install rbenv
+brew install ruby-build
+
+brew cask install rubymine
+brew cask install atom
+brew cask install karabiner
+brew cask install google-japanese-ime
+brew cask install dropbox
+
+brew cleanup
