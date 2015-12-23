a7688e03cebdc4a1ba1e004b198f4562d412388d
junsumida <sekitoh.rh@gmail.com>
2015-12-23 22:54:05 +0900

    update for my mbp 13

--- brewfile.sh
+++ brewfile.sh
@@ -4,16 +4,12 @@ brew tap phinze/homebrew-cask
 brew install brew-cask
 
 brew install wget
-brew install redis
-brew install mysql --cc=llvm-gcc
 brew install tree
 brew install tig
 brew install zsh
 brew install hub
+brew install vim
 brew install the_silver_searcher
-brew install mysql
-brew install memcached
-brew install colordiff
 
 # commandline tools
 brew tap peco/peco
@@ -26,31 +22,12 @@ brew install ruby-build
 
 # editors
 brew cask install rubymine
-brew cask install intellij-idea-ce
 brew cask install atom
 
 brew cask install karabiner
-brew cask install google-japanese-ime
 brew cask install dropbox
 brew cask install virtualbox 
 brew cask install vagrant
-brew cask install hipchat
-brew cask install amethyst
-
-# Docker
-brew install boot2docker
-brew install docker
-
-# Haskell
-# for scotty on heroku
-brew install postgresql
-
-# heroku
-brew cask install heroku-toolbelt
-
-# scala
-brew install sbt
-brew install scala
 
 # ansible
 brew install ansible
